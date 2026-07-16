7601db7764fa3737eb170882d0e6035be386eff3
Snizhana Tanasiv <snizok.t33@gmail.com>
2026-07-16 18:54:51 +0200

    Complete homework 01-express

---
+++ src/server.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const pinoHttp = require('pino-http');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(pinoHttp());
+
+// Routes
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+// 404 Middleware
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+// Error Middleware
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
