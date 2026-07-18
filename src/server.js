f55d69a3970cdd8be401902a16c9a625e52f9337
Snizhana Tanasiv <snizok.t33@gmail.com>
2026-07-18 18:00:42 +0200

    Complete mongodb homework

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { notesRouter } from './routes/notesRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger);
+app.use(cors());
+app.use(express.json());
+
+app.use(notesRouter);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+const bootstrap = async () => {
+  await connectMongoDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+bootstrap();
